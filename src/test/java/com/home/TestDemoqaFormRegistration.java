59102bf99d291952fb680e29bafbc0fcacf24a19
AslanMus <asmus01@mail.ru>
2024-05-03 16:57:34 +0300

    ok

--- src/test/java/com/home/TestDemoqaFormRegistration.java
+++ src/test/java/com/home/TestDemoqaFormRegistration.java
@@ -1,17 +1,9 @@
 package com.home;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.BeforeAll;
+
 import org.junit.jupiter.api.Test;
-import pages.CalendarComponent;
 import pages.TestPageDemoqa;
 
-import java.io.FilenameFilter;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
 
 public class TestDemoqaFormRegistration extends TestBase {
 
